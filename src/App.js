33eb539d1be6abb0f2ab730f95b210fd3fc529d4
Robert Stepanov <robertdstepanov@gmail.com>
2019-12-18 16:32:03 -0500

    MVP Met

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MemberForm from "./components/MemberForm";
+import Members from "./components/Members";
+import "./App.css";
 
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const addNewMember = member => {
+    const newMember = {
+      id: Date.now(),
+      fullName: member.fullName,
+      email: member.email,
+      role: member.role
+    };
+    setMembers([...members, newMember]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Members</h1>
+      <MemberForm addNewMember={addNewMember} />
+      <Members members={members} />
     </div>
   );
 }
